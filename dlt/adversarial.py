f1c5ffd2ff117f5da51aeb336e3f5351565ccbc5
Hao Zhang <albert.zhang.hit@gmail.com>
2019-11-08 13:27:59 -0800

    Revert "Revert "more smart way""
    This reverts commit 21282ad1769ca809602f9cc0601e063bc337c0fc.

--- dlt/adversarial.py
+++ dlt/adversarial.py
@@ -122,7 +122,8 @@ class AdversarialTraining(BaseTraining):
 				current_iter = (epoch - 1) * num_batches + i
 
 				data, label = data.to(self._device), label.to(self._device)	
-				data = self._perturb(data, label)
+				if not self._train_flag:
+					data = self._perturb(data, label)
 
 				y = self._forward_op(data)			
 				logits = self._net(y)
