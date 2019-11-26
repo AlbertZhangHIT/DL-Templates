1f2a3e3c7878f5b3af059a7e52acf0a47e2d4d50
Hao Zhang <albert.zhang.hit@gmail.com>
2019-11-26 11:49:29 -0800

    Update __init__.py

--- dlt/__init__.py
+++ dlt/__init__.py
@@ -2,11 +2,7 @@ from .base import Training, BaseTraining
 from .common import PlainTraining
 from .adversarial import AdversarialTraining
 from .free_adversarial import FreeAdversarialBaseTraining, FreeAdversarialTraining
-from .perturbations import (L1Perturbation,
-		FGSM, L2Perturbation, LInfPerturbation, 
-		LinfIterativePerturbation,
-		ProjectedGradientDescent, PGD, L2IterativePerturbation, 
-		L1IterativePerturbation)
-from .perturbations import fgsm
 
-from .optim import LSSGD
+from . import optim
+from . import classify
+from . import perturbations
