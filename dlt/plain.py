fefe3fb83d17f9709d2a3353950890997f0e4b7a
Hao Zhang <albert.zhang.hit@gmail.com>
2021-01-17 11:49:41 +0800

    Update plain.py

--- dlt/plain.py
+++ dlt/plain.py
@@ -49,12 +49,8 @@ class PlainTraining(BaseTraining):
 				lr = self._optimizer.defaults['lr']
 
 			avg_loss.update(current_loss.item(), data.size(0))
-			current_measure = self._measure(logits, label)
+			current_measure = self._measure(logits.detach(), label.detach())
 			avg_measre.update(current_measure, data.size(0))
-
-			del current_loss
-			del current_measure
-			del logits
 
 			#Update the progress
 			data_stream.set_description((
@@ -102,7 +98,7 @@ class PlainTraining(BaseTraining):
 				logits = self._net(y)
 				current_loss = self._loss_fun(logits, label)
 				avg_loss.update(current_loss.item(), data.size(0))
-				current_measure = self._measure(logits, label)
+				current_measure = self._measure(logits.detach(), label.detach())
 				avg_measre.update(current_measure, data.size(0))
 				#Update the progress
 				data_stream.set_description((
