abb1543944e6f4caf95aeaa76b1961a148fd805d
Hao Zhang <albert.zhang.hit@gmail.com>
2021-01-19 01:21:33 +0800

    Update measures.py
    fix batch measure mean to sum

--- dlt/measures.py
+++ dlt/measures.py
@@ -6,14 +6,14 @@ def batchSNR(recon, clean, data_range=1.):
 	mse = (clean - recon).view(batch_size, -1).pow(2).mean(dim=1)
 	snrs = 10. * torch.log10(norm/(mse+1e-20))
 
-	return snrs.mean()
+	return snrs.sum()
 
 def batchPSNR(recon, clean, data_range=1.):
 	batch_size = recon.size(0)
 	mse = (clean - recon).view(batch_size, -1).pow(2).mean(dim=1)
 	psnrs = 10. * torch.log10(data_range**2 / (mse+1e-20))
 
-	return psnrs.mean()
+	return psnrs.sum()
 
 def classPredict(prediction, label):
 	_, predicted = torch.max(prediction.data, 1)
